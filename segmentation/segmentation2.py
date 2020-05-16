53c5c97d446a0633a33beb178f5312d8843a9965
Thomas Jacob <tjacob7911@gmail.com>
2020-05-16 18:40:10 +0530

    Some additional changes

--- segmentation/segmentation2.py
+++ segmentation/segmentation2.py
@@ -1,13 +1,10 @@
 import cv2 as cv
+import os
 import numpy as np
 import matplotlib.pyplot as plt
 import random as rnd
 
-rnd.seed(1)
-#fac = 100
-
 def thresh_callback(val):
-    global fac
     threshold = val
 
     canny_img = cv.Canny(img_gray, threshold, threshold*2)
@@ -20,8 +17,6 @@ def thresh_callback(val):
         contours_poly[i] = cv.approxPolyDP(c, 3, True)
         bound_rect[i] = cv.boundingRect(contours_poly[i])
     
-    #h, w = canny_img.shape[:2]
-    #bound_rect_new, contours_poly_new, contours_poly_rep = find_valid_rect(contours_poly, bound_rect, h, w, fac)
     contours_poly_new, bound_rect_new, contours_poly_rep = rect_filter(contours_poly, bound_rect)
     drawing = np.zeros((canny_img.shape[0], canny_img.shape[1], 3), dtype = np.uint8)
 
@@ -34,14 +29,6 @@ def thresh_callback(val):
         cv.drawContours(drawing, contours_poly_rep, j, (255, 255, 255))
 
     cv.imshow('Contours', drawing)
-
-'''
-def fac_callback(pos):
-    global fac
-    fac = pos
-    val = cv.getTrackbarPos('Threshold', 'Source')
-    thresh_callback(val)
-'''
 
 def intersection(a,b):
     x = max(a[0], b[0])
@@ -62,38 +49,6 @@ def iou(a,b):
     inter_area = intersection(a,b)
     iou = inter_area/union_area
     return iou
-
-'''
-def find_valid_rect(poly_contours, bound_rect, h, w, fac1):
-    contours_rep = []
-    rect_rep = []
-    contours_new = []
-    rect_new = []
-    for i in range(len(poly_contours)):
-        #rect = bound_rect[i]
-        for j in range(len(bound_rect)):
-            if i == j:
-                continue
-            
-            x1, y1, w1, h1 = bound_rect[i][0], bound_rect[i][1], bound_rect[i][2], bound_rect[i][3]
-            x2, y2, w2, h2 = bound_rect[j][0], bound_rect[j][1], bound_rect[j][2], bound_rect[j][3]
-
-            if intersection((x1,y1,w1,h1), (x2,y2,w2,h2)) == w2 * h2 and w2 * h2 < (w * h)/fac1:
-                contours_rep.append(poly_contours[j])
-                rect_rep.append(bound_rect[j])
-    
-    for k in range(len(poly_contours)):
-        repeat = False
-        for l in range(len(contours_rep)):
-            if bound_rect[k] == rect_rep[l]:
-                repeat = True
-
-        if not repeat:
-            contours_new.append(poly_contours[k])
-            rect_new.append(bound_rect[k])
-
-    return rect_new, contours_new, contours_rep                 
-'''
 
 def rect_filter(contours_poly, bound_rect):
     contours_poly_new = []
@@ -140,11 +95,11 @@ def rect_filter(contours_poly, bound_rect):
 
 
 
+#IMG_PATH = os.path.join(os.path.dirname(__file__), "int_img6.jpg")
 IMG_PATH = '/Users/thomasmacbookair/Desktop/ITSP/int_img6.jpg'
 kernel = np.ones((3,3), np.uint8)
 
 img = cv.imread(IMG_PATH) 
-#img = cv.resize(img, None, fx = 0.25, fy = 0.25)
 img = cv.resize(img, (650, 650), interpolation = cv.INTER_CUBIC)
 _, img_gray = cv.threshold(img, 50, 255, cv.THRESH_BINARY_INV)
 img_gray = cv.dilate(img_gray, kernel, iterations = 10)
@@ -155,10 +110,7 @@ cv.imshow('GRAY', img_gray)
 
 def_thresh = 100
 max_thresh = 255
-#def_fac = 100
 cv.createTrackbar('Threshold', 'Source', def_thresh, max_thresh, thresh_callback)
-#cv.createTrackbar('Factor', 'Source', def_fac, 5000, fac_callback)
-#fac_callback(def_fac)
 thresh_callback(def_thresh)
 
 cv.waitKey()
